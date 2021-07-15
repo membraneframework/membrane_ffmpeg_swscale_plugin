9fc53cd909a5b5b5a17406ebe88a121bb0735a59
sheldak <samuel.heldak@gmail.com>
2021-07-15 16:02:20 +0200

    exclude generated code from hex package

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Membrane.FFmpeg.SWScale.Mixfile do
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
       },
-      files: ["lib", "mix.exs", "README*", "LICENSE*", ".formatter.exs", "bundlex.exs", "c_src"]
+      files: ["lib", "mix.exs", "README*", "LICENSE*", ".formatter.exs", "bundlex.exs", "c_src"],
+      exclude_patterns: [~r"c_src/.*/_generated.*"]
     ]
   end
 
