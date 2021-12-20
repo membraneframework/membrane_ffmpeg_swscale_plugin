a8b1334eec0485e4772ff97ea34e8814a314bd5c
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2021-12-20 14:01:05 +0100

    Make scale/3 a dirty nif

--- c_src/membrane_ffmpeg_swscale_plugin/scaler.spec.exs
+++ c_src/membrane_ffmpeg_swscale_plugin/scaler.spec.exs
@@ -3,10 +3,13 @@ module Membrane.FFmpeg.SWScale.Scaler.Native
 state_type "State"
 
 spec create(
-    input_width :: int,
-    input_height :: int,
-    output_width :: int,
-    output_height :: int
-) :: {:ok :: label, state} | {:error :: label, reason :: atom}
+       input_width :: int,
+       input_height :: int,
+       output_width :: int,
+       output_height :: int
+     ) :: {:ok :: label, state} | {:error :: label, reason :: atom}
 
-spec scale(payload, use_shm :: bool, state) :: {:ok :: label, payload} | {:error :: label, reason :: atom}
+spec scale(payload, use_shm :: bool, state) ::
+       {:ok :: label, payload} | {:error :: label, reason :: atom}
+
+dirty :cpu, scale: 3
