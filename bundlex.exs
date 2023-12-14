cb399c9596ebbf3eaf72062d46f6fa75fbae61ff
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2023-12-14 11:41:24 +0100

    Start using Precompiled Dependency Provider (#29)

--- bundlex.exs
+++ bundlex.exs
@@ -1,30 +1,6 @@
 defmodule Membrane.FFmpeg.SWScale.BundlexProject do
   use Bundlex.Project
 
-  defp get_ffmpeg_url() do
-    membrane_precompiled_url_prefix =
-      "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg"
-
-    case Bundlex.get_target() do
-      %{architecture: "aarch64", os: "linux"} ->
-        {:precompiled,
-         "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linuxarm64-gpl-shared-6.0.tar.xz"}
-
-      %{os: "linux"} ->
-        {:precompiled,
-         "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-shared-6.0.tar.xz"}
-
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{membrane_precompiled_url_prefix}_macos_intel.tar.gz"}
-
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{membrane_precompiled_url_prefix}_macos_arm.tar.gz"}
-
-      _other ->
-        nil
-    end
-  end
-
   def project do
     [
       natives: natives()
@@ -37,7 +13,11 @@ defmodule Membrane.FFmpeg.SWScale.BundlexProject do
         interface: :nif,
         sources: ["scaler/scaler.c"],
         os_deps: [
-          {[get_ffmpeg_url(), :pkg_config], ["libswscale", "libavutil"]}
+          ffmpeg: [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+             ["libswscale", "libavutil"]},
+            {:pkg_config, ["libswscale", "libavutil"]}
+          ]
         ],
         preprocessor: Unifex
       ],
@@ -45,7 +25,11 @@ defmodule Membrane.FFmpeg.SWScale.BundlexProject do
         interface: :nif,
         sources: ["pixel_format_converter/converter.c"],
         os_deps: [
-          {[get_ffmpeg_url(), :pkg_config], ["libswscale", "libavutil"]}
+          ffmpeg: [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+             ["libswscale", "libavutil"]},
+            {:pkg_config, ["libswscale", "libavutil"]}
+          ]
         ],
         preprocessor: Unifex
       ]
